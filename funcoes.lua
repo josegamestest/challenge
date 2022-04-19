b26adbaa5eba7730c4b69e4c46f310ef117717d4
joseanastacio <joseanastaciocastilho@gmail.com>
2022-04-19 11:11:49 -0300

    Initial commit

---
+++ funcoes.lua
@@ -0,0 +1,7 @@
+function mine_wingsmoveiten(self,pos,dtime)
+		
+end
+
+function mine_wingsmoveiten2(self,pos,dtime)
+		
+end
---
+++ init.lua
@@ -0,0 +1,8 @@
+challenge = {}
+
+--arquivos
+--fazer
+dofile(minetest.get_modpath("challenge") .. DIR_DELIM .. "itens.lua") 
+dofile(minetest.get_modpath("challenge") .. DIR_DELIM .. "funcoes.lua") 
+
+
