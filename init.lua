f478d4fd18d5a31d8a03c55660e6abd73a0b095a
joseanastacio <joseanastaciocastilho@gmail.com>
2022-04-25 19:44:00 -0300

    removed general debug message removed general debug messageUpdate itens.lua
    translate
    When the player passes through the circle it shows the name of the player in the circle that passed through it

--- init.lua
+++ init.lua
@@ -2,7 +2,10 @@ challenge = {}
 
 --arquivos
 --fazer
-dofile(minetest.get_modpath("challenge") .. DIR_DELIM .. "itens.lua") 
-dofile(minetest.get_modpath("challenge") .. DIR_DELIM .. "funcoes.lua") 
+dofile(minetest.get_modpath("challenge") .. DIR_DELIM .. "chekpoints.lua") 
+dofile(minetest.get_modpath("challenge") .. DIR_DELIM .. "flag_point.lua") 
 
+dofile(minetest.get_modpath("challenge") .. DIR_DELIM .. "objetos.lua") 
+
+dofile(minetest.get_modpath("challenge") .. DIR_DELIM .. "move_tool.lua") 
 
